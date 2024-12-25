ee25c50a62034c74be3d27e992a28a54a79ff958
alexnfk <alexnfk@gmail.com>
2024-12-25 20:41:16 +0000

    Ported code for 3d rendering of a sphere, z buffer needs fixing to deal with board memory constraints

--- main/main.ino
+++ main/main.ino
@@ -1,148 +1,122 @@
-/*
- An example analogue clock using a TFT LCD screen to show the time
- use of some of the drawing commands with the library.
+// This sketch includes a function to draw circle segments
+// for pie charts in 1 degree increments
 
- For a more accurate clock, it would be better to use the RTClib library.
- But this is just a demo. 
- 
- This sketch uses font 4 only.
-
- Make sure all the display driver and pin connections are correct by
- editing the User_Setup.h file in the TFT_eSPI library folder.
-
- #########################################################################
- ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
- #########################################################################
- 
- Based on a sketch by Gilchrist 6/2/2014 1.0
- */
-
-#include <SPI.h>
 #include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-#define TFT_GREY 0x5AEB
+#define screenX 240
+#define screenY 320
 
-TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
-float sx = 0, sy = 1, mx = 1, my = 0, hx = -1, hy = 0;    // Saved H, M, S x & y multipliers
-float sdeg=0, mdeg=0, hdeg=0;
-uint16_t osx=120, osy=120, omx=120, omy=120, ohx=120, ohy=120;  // Saved H, M, S x & y coords
-uint16_t x0=0, x1=0, yy0=0, yy1=0;
-uint32_t targetTime = 0;                    // for next 1 second timeout
+typedef struct point
+{
+    int x;
+    int y;
+    int z;
+} screen;
 
-static uint8_t conv2d(const char* p); // Forward declaration needed for IDE 1.6.x
-uint8_t hh=conv2d(__TIME__), mm=conv2d(__TIME__+3), ss=conv2d(__TIME__+6);  // Get H, M, S from compile time
+screen S[screenX][screenY];
 
-bool initial = 1;
+void setup(void)
+{
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+}
 
-void setup(void) {
-  tft.init();
-  tft.setRotation(0);
-  
-  //tft.fillScreen(TFT_BLACK);
-  //tft.fillScreen(TFT_RED);
-  //tft.fillScreen(TFT_GREEN);
-  //tft.fillScreen(TFT_BLUE);
-  //tft.fillScreen(TFT_BLACK);
-  tft.fillScreen(TFT_GREY);
-  
-  tft.setTextColor(TFT_WHITE, TFT_GREY);  // Adding a background colour erases previous text automatically
-  
-  // Draw clock face
-  tft.fillCircle(120, 120, 118, TFT_GREEN);
-  tft.fillCircle(120, 120, 110, TFT_BLACK);
+float sigmoid(float val)
+{
+    float sig = 1/(1+(pow(M_E,(-val))));
+    return sig;
+}
 
-  // Draw 12 lines
-  for(int i = 0; i<360; i+= 30) {
-    sx = cos((i-90)*0.0174532925);
-    sy = sin((i-90)*0.0174532925);
-    x0 = sx*114+120;
-    yy0 = sy*114+120;
-    x1 = sx*100+120;
-    yy1 = sy*100+120;
+unsigned char z2lum(int z)
+{
+    unsigned char lums[13] = {'@','$','#','*','!','=',';',':','~','-',',','.',' '};
+    float a = 12*sigmoid(z);
+    z = round(a);
+    char lum = lums[z];
+    //printf("%f\n",a);
+    return lum;
+}
 
-    tft.drawLine(x0, yy0, x1, yy1, TFT_GREEN);
-  }
-
-  // Draw 60 dots
-  for(int i = 0; i<360; i+= 6) {
-    sx = cos((i-90)*0.0174532925);
-    sy = sin((i-90)*0.0174532925);
-    x0 = sx*102+120;
-    yy0 = sy*102+120;
-    // Draw minute markers
-    tft.drawPixel(x0, yy0, TFT_WHITE);
+void drawPoint(float x, float y, float z)
+{
+    struct point newPoint;
+    //newPoint.x = round(((K1*x)/(K2+z)));
+    //newPoint.y = round(((K1*y)/(K2+z)));
+    newPoint.x = round(x);
+    newPoint.y = round(y);
+    newPoint.z = round(z);
     
-    // Draw main quadrant dots
-    if(i==0 || i==180) tft.fillCircle(x0, yy0, 2, TFT_WHITE);
-    if(i==90 || i==270) tft.fillCircle(x0, yy0, 2, TFT_WHITE);
-  }
-
-  tft.fillCircle(120, 121, 3, TFT_WHITE);
-
-  // Draw text at position 120,260 using fonts 4
-  // Only font numbers 2,4,6,7 are valid. Font 6 only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : . - a p m
-  // Font 7 is a 7 segment font and only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : .
-  tft.drawCentreString("Time flies",120,260,4);
-
-  targetTime = millis() + 1000; 
+    struct point current = S[newPoint.x][newPoint.y];
+    
+    /*
+    If coordinates are positive and within the bounds of the screen, add the point to the screen array
+    */
+    if(((0<=newPoint.x) && (newPoint.x<screenX)) && ((0<=newPoint.y) && (newPoint.y<screenY)) && (newPoint.z < current.z))
+    {
+      S[newPoint.x][newPoint.y] = newPoint;
+      tft.drawPixel(newPoint.x,newPoint.y,0xFF0000);
+    }
 }
 
-void loop() {
-  if (targetTime < millis()) {
-    targetTime += 1000;
-    ss++;              // Advance second
-    if (ss==60) {
-      ss=0;
-      mm++;            // Advance minute
-      if(mm>59) {
-        mm=0;
-        hh++;          // Advance hour
-        if (hh>23) {
-          hh=0;
+void initScreenArray()
+{
+    for(int i = (screenY-1); i>-1;i--)
+    {
+        for(int j = 0; j<screenX; j++)
+        {   
+            struct point newPoint;
+            newPoint.x = j;
+            newPoint.y = i;
+            newPoint.z = 100;
+
+            if(((0<=newPoint.x) && (newPoint.x<screenX)) && ((0<=newPoint.y) && (newPoint.y<screenY)))
+            {
+                S[j][i] = newPoint;
+            }
         }
-      }
     }
-
-    // Pre-compute hand degrees, x & y coords for a fast screen update
-    sdeg = ss*6;                  // 0-59 -> 0-354
-    mdeg = mm*6+sdeg*0.01666667;  // 0-59 -> 0-360 - includes seconds
-    hdeg = hh*30+mdeg*0.0833333;  // 0-11 -> 0-360 - includes minutes and seconds
-    hx = cos((hdeg-90)*0.0174532925);    
-    hy = sin((hdeg-90)*0.0174532925);
-    mx = cos((mdeg-90)*0.0174532925);    
-    my = sin((mdeg-90)*0.0174532925);
-    sx = cos((sdeg-90)*0.0174532925);    
-    sy = sin((sdeg-90)*0.0174532925);
-
-    if (ss==0 || initial) {
-      initial = 0;
-      // Erase hour and minute hand positions every minute
-      tft.drawLine(ohx, ohy, 120, 121, TFT_BLACK);
-      ohx = hx*62+121;    
-      ohy = hy*62+121;
-      tft.drawLine(omx, omy, 120, 121, TFT_BLACK);
-      omx = mx*84+120;    
-      omy = my*84+121;
-    }
-
-      // Redraw new hand positions, hour and minute hands not erased here to avoid flicker
-      tft.drawLine(osx, osy, 120, 121, TFT_BLACK);
-      osx = sx*90+121;    
-      osy = sy*90+121;
-      tft.drawLine(osx, osy, 120, 121, TFT_RED);
-      tft.drawLine(ohx, ohy, 120, 121, TFT_WHITE);
-      tft.drawLine(omx, omy, 120, 121, TFT_WHITE);
-      tft.drawLine(osx, osy, 120, 121, TFT_RED);
-
-    tft.fillCircle(120, 121, 3, TFT_RED);
-  }
 }
 
-static uint8_t conv2d(const char* p) {
-  uint8_t v = 0;
-  if ('0' <= *p && *p <= '9')
-    v = *p - '0';
-  return 10 * v + *++p - '0';
+void sphere(float Cx, float Cy, float Cz, float rad)
+{
+    float x = rad;
+    float z = 0;
+    float y = 0;
+
+    //rotation around y axis
+    for(float theta = 0; theta<(2*M_PI); theta+=M_PI/180)
+    {
+        float tX = x*cosf(theta) + z*sinf(theta);
+        float tY = y;
+        float tZ = z*cosf(theta) - x*sinf(theta);
+        //rotation around z axis
+        for(float phi = 0; phi<(2*M_PI); phi+=M_PI/180)
+        {
+            //code for circle but in 3d
+            float pX = tX*cosf(phi) - tY*sinf(phi);
+            float pY = tX*sinf(phi) + tY*cosf(phi);
+            float pZ = tZ;
+
+            //z2lum(round(pZ+Cz))
+            drawPoint(pX+Cx,pY+Cy,pZ+Cz);
+        }
+        //printScreen();
+    }
 }
+
+void loop() 
+{
+  //tft.drawCircle(100, 100, 10, 0xFF0000);
+  sphere(100,100,20,50);
+}
+
+
+
+
 
