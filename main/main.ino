835b0185f6a7ea1ca045e05f204957579b16294c
alexnfk <alexnfk@gmail.com>
2025-01-02 16:28:39 +0000

    Implemented Z buffer, changes to sphere coordinate calculation, crashes when drawing larger spheres due to overflow, optimisation needed, shading to be implemented.

--- main/main.ino
+++ main/main.ino
@@ -1,24 +1,21 @@
-// This sketch includes a function to draw circle segments
-// for pie charts in 1 degree increments
-
 #include <TFT_eSPI.h> // Hardware-specific library
 #include <SPI.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+#include <utility>
+using namespace std;
+
 #define screenX 240
 #define screenY 320
 
+#define K1 120
+#define K2 60
+
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
-typedef struct point
-{
-    int x;
-    int y;
-    int z;
-} screen;
-
-screen S[screenX][screenY];
+vector<pair<int,int>> projectedXYs;
+vector<float> zBuffer;
 
 void setup(void)
 {
@@ -33,6 +30,7 @@ float sigmoid(float val)
     return sig;
 }
 
+/*
 unsigned char z2lum(int z)
 {
     unsigned char lums[13] = {'@','$','#','*','!','=',';',':','~','-',',','.',' '};
@@ -43,77 +41,55 @@ unsigned char z2lum(int z)
     return lum;
 }
 
+*/
 void drawPoint(float x, float y, float z)
 {
-    struct point newPoint;
-    //newPoint.x = round(((K1*x)/(K2+z)));
-    //newPoint.y = round(((K1*y)/(K2+z)));
-    newPoint.x = round(x);
-    newPoint.y = round(y);
-    newPoint.z = round(z);
-    
-    struct point current = S[newPoint.x][newPoint.y];
-    
-    /*
-    If coordinates are positive and within the bounds of the screen, add the point to the screen array
-    */
-    if(((0<=newPoint.x) && (newPoint.x<screenX)) && ((0<=newPoint.y) && (newPoint.y<screenY)) && (newPoint.z < current.z))
-    {
-      S[newPoint.x][newPoint.y] = newPoint;
-      tft.drawPixel(newPoint.x,newPoint.y,0xFF0000);
-    }
-}
+  int projX = round((K1*x)/(K2 + z));
+  int projY = round((K1*y)/(K2 + z));
+  pair<int,int> xy;
+  xy.first = projX;
+  xy.second = projY;
+  
+  auto it = find(projectedXYs.begin(), projectedXYs.end(), xy);
+  int pos = distance(projectedXYs.begin(), it);
 
-void initScreenArray()
-{
-    for(int i = (screenY-1); i>-1;i--)
+  if(pos<projectedXYs.size())
+  {
+    float currentZ = zBuffer[pos];
+    if(currentZ>z)
     {
-        for(int j = 0; j<screenX; j++)
-        {   
-            struct point newPoint;
-            newPoint.x = j;
-            newPoint.y = i;
-            newPoint.z = 100;
-
-            if(((0<=newPoint.x) && (newPoint.x<screenX)) && ((0<=newPoint.y) && (newPoint.y<screenY)))
-            {
-                S[j][i] = newPoint;
-            }
-        }
+      zBuffer.at(pos) = z;
+      projectedXYs.at(pos) = xy;
+      tft.drawPixel(projX,projY,0xFF0000);
     }
+  }
+  else
+  {
+    projectedXYs.push_back(xy);
+    zBuffer.push_back(z);
+    tft.drawPixel(projX,projY,0xFF0000);
+  }
 }
 
 void sphere(float Cx, float Cy, float Cz, float rad)
 {
-    float x = rad;
-    float z = 0;
-    float y = 0;
-
-    //rotation around y axis
-    for(float theta = 0; theta<(2*M_PI); theta+=M_PI/180)
+  for(float phi = 0; phi<M_PI; phi+= M_PI/180)
+  {
+    for(float theta = 0; theta<2*M_PI; theta+= M_PI/180)
     {
-        float tX = x*cosf(theta) + z*sinf(theta);
-        float tY = y;
-        float tZ = z*cosf(theta) - x*sinf(theta);
-        //rotation around z axis
-        for(float phi = 0; phi<(2*M_PI); phi+=M_PI/180)
-        {
-            //code for circle but in 3d
-            float pX = tX*cosf(phi) - tY*sinf(phi);
-            float pY = tX*sinf(phi) + tY*cosf(phi);
-            float pZ = tZ;
+      float x = rad*sinf(phi) * cosf(theta);
+      float y = rad*sinf(phi) * sinf(theta);
+      float z = rad*cosf(phi);
 
-            //z2lum(round(pZ+Cz))
-            drawPoint(pX+Cx,pY+Cy,pZ+Cz);
-        }
-        //printScreen();
+      drawPoint(x+Cx,y+Cy,z+Cz);
     }
+  }
 }
 
 void loop() 
 {
   //tft.drawCircle(100, 100, 10, 0xFF0000);
-  sphere(100,100,20,50);
+  sphere(100,100,10,10);
 }
 
 
