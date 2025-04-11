f35adc628632a2e1d1eea2793eaf1e8625d96f9f
alexnfk <alexnfk@gmail.com>
2025-04-11 19:20:39 +0100

    First half of shading implementation

--- main/main.ino
+++ main/main.ino
@@ -44,10 +44,10 @@ void clearZBuffer()
     zBuffer[i] = 255;
 }
 
-void drawPoint(float x, float y, float z)
+void drawPoint(float x, float y, float z, int color[3])
 {
-  int projX = round((K1X*x)/(K2 + z));
-  int projY = round((K1Y*y)/(K2 + z));
+  int projX = round((K1X*x)/(K2 + z)) + (screenX/2);
+  int projY = round((K1Y*y)/(K2 + z)) + (screenY/2);
   
   if(projX >=0 && projX < screenX && projY >=0 && projY < screenY)
   {
@@ -56,24 +56,36 @@ void drawPoint(float x, float y, float z)
     if(zNorm<zBuffer[index])
       {
         zBuffer[index] = zNorm;
-        tft.drawPixel(projX,projY,tft.color565(0, 0, 255));
+        tft.drawPixel(projX,projY,tft.color565(color[0],color[1],color[2]));
       }
   }
   
   
 }
 
+//Given a vector light direction, surface normal vector and a colour, computed the dot product of the vectors and scales the colour to shade the pixel
+int* shadePoint(float lightDirVector[3], float normalVector[3], int color[3])
+{
+  float dp = (lightDirVector[0]*normalVector[0]) + (lightDirVector[1]*normalVector[1]) + (lightDirVector[2]*normalVector[2]);
+  int red = round(constrain((sigmoid(dp) * color[0]), 0, 255));
+  int green = round(constrain((sigmoid(dp) * color[1]), 0, 255));
+  int blue = round(constrain((sigmoid(dp) * color[2]), 0, 255));
+
+  return {red,green,blue};
+}
+
 void sphere(int Cx, int Cy, int Cz, int rad)
 {
-  for(float phi = 0; phi<M_PI; phi+= M_PI/90)
+  for(float phi = 0; phi<M_PI; phi+= M_PI/180)
   {
-    for(float theta = 0; theta<2*M_PI; theta+= M_PI/90)
+    for(float theta = 0; theta<2*M_PI; theta+= M_PI/180)
     {
       float x = rad*sinf(phi) * cosf(theta);
       float y = rad*sinf(phi) * sinf(theta);
       float z = rad*cosf(phi);
 
-      drawPoint(x+Cx,y+Cy,z+Cz);
+      int color[3] = shadePoint();
+      drawPoint(x+Cx,y+Cy,z+Cz,color);
     }
   }
 }
@@ -81,11 +93,7 @@ void sphere(int Cx, int Cy, int Cz, int rad)
 void loop() 
 {
   //tft.drawCircle(100, 100, 10, tft.color565(0, 0, 255));
-  sphere(120,Y,20,30);
-  delay(30);
-  tft.fillScreen(TFT_BLACK);
-  clearZBuffer();
-  Y++;
+  sphere(X,Y,20,30);
 }
 
 
